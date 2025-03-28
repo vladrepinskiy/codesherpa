a8142c6b9682671c52a3ae1d5163f2643b3a50f0
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2025-03-28 17:25:37 +0100

    Update shadcn imports for toast and alert dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      <Toaster />
     </html>
   );
 }
