00713cb3f413d5d07db65482886b719eaece17bb
aetherjs <devrepinsky@gmail.com>
2025-04-01 15:35:07 +0200

    Try and force amplify to make env variables available server side

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    CHROMA_DB_URL: process.env.CHROMA_DB_URL,
+    CHROMA_RESULTS_NUMBER: process.env.CHROMA_RESULTS_NUMBER,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
