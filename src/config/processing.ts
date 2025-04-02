6a551c20d5ea437b575e36e25391f51cc1cdd306
aetherjs <devrepinsky@gmail.com>
2025-04-02 12:15:35 +0200

    Modify batch sizes

--- src/config/processing.ts
+++ src/config/processing.ts
@@ -1,2 +1,2 @@
 export const CHUNK_SIZE = 1000;
-export const BATCH_SIZE = 5;
+export const BATCH_SIZE = 2; // Experimenting with in in my current EC2 instance, setting a small batch just TO BE SAFE.
